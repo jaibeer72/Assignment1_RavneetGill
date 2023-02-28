52768034911ed389406cd1bb387ad6c2c6b51aa2
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-02-28 19:12:03 +0000

    Initial Commit

---
+++ Assignment1_RavneetGill/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment1_RavneetGill
+//
+//  Created by Jaibeer Dugal on 28/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
